f8f0b668afa048c5c1f43203757dcb3346e31ee8
Muhammad Owais Ansari <owais.ansaricr7@gmail.com>
2023-08-06 23:10:02 +0500

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+// import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { RoughEase } from 'gsap/all'; // Import RoughEase from react-gsap
+import { Linear, Sine, Bounce, Elastic, Power0 } from 'gsap/all'; // Import necessary easing functions
+import Home from './components/Home';
+
+
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+     <>
+    
+      <Home/>
+     </>
+    
   );
-}
+};
 
 export default App;
